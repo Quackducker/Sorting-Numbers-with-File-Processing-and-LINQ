dc65053863d9aa1d84fa528351102530305ced8d
QuackDucker <Tarantetikstayo@gmail.com>
2025-11-07 08:28:12 +0800

    Put in a Clear Button

--- Sorting Numbers with File Processing and LINQ/Form1.Designer.vb
+++ Sorting Numbers with File Processing and LINQ/Form1.Designer.vb
@@ -22,32 +22,33 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.Button1 = New System.Windows.Forms.Button()
+        Me.btnWrite = New System.Windows.Forms.Button()
         Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
-        Me.Button2 = New System.Windows.Forms.Button()
+        Me.btnRead = New System.Windows.Forms.Button()
         Me.ListBox1 = New System.Windows.Forms.ListBox()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.Button3 = New System.Windows.Forms.Button()
+        Me.btnSort = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
-        'Button1
+        'btnWrite
         '
-        Me.Button1.Location = New System.Drawing.Point(374, 96)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(104, 29)
-        Me.Button1.TabIndex = 0
-        Me.Button1.Text = "Write"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.btnWrite.Location = New System.Drawing.Point(374, 96)
+        Me.btnWrite.Name = "btnWrite"
+        Me.btnWrite.Size = New System.Drawing.Size(104, 29)
+        Me.btnWrite.TabIndex = 0
+        Me.btnWrite.Text = "Write"
+        Me.btnWrite.UseVisualStyleBackColor = True
         '
-        'Button2
+        'btnRead
         '
-        Me.Button2.Location = New System.Drawing.Point(374, 163)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(120, 26)
-        Me.Button2.TabIndex = 1
-        Me.Button2.Text = "Read"
-        Me.Button2.UseVisualStyleBackColor = True
+        Me.btnRead.Location = New System.Drawing.Point(374, 163)
+        Me.btnRead.Name = "btnRead"
+        Me.btnRead.Size = New System.Drawing.Size(120, 26)
+        Me.btnRead.TabIndex = 1
+        Me.btnRead.Text = "Read"
+        Me.btnRead.UseVisualStyleBackColor = True
         '
         'ListBox1
         '
@@ -73,26 +74,36 @@ Partial Class Form1
         Me.TextBox1.Size = New System.Drawing.Size(108, 20)
         Me.TextBox1.TabIndex = 4
         '
-        'Button3
+        'btnSort
         '
-        Me.Button3.Location = New System.Drawing.Point(399, 279)
-        Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(167, 36)
-        Me.Button3.TabIndex = 5
-        Me.Button3.Text = "Start"
-        Me.Button3.UseVisualStyleBackColor = True
+        Me.btnSort.Location = New System.Drawing.Point(399, 279)
+        Me.btnSort.Name = "btnSort"
+        Me.btnSort.Size = New System.Drawing.Size(167, 36)
+        Me.btnSort.TabIndex = 5
+        Me.btnSort.Text = "Sort"
+        Me.btnSort.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(209, 332)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(131, 28)
+        Me.btnClear.TabIndex = 6
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnSort)
         Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.ListBox1)
-        Me.Controls.Add(Me.Button2)
-        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.btnRead)
+        Me.Controls.Add(Me.btnWrite)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
@@ -100,11 +111,12 @@ Partial Class Form1
 
     End Sub
 
-    Friend WithEvents Button1 As Button
+    Friend WithEvents btnWrite As Button
     Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
-    Friend WithEvents Button2 As Button
+    Friend WithEvents btnRead As Button
     Friend WithEvents ListBox1 As ListBox
     Friend WithEvents Label1 As Label
     Friend WithEvents TextBox1 As TextBox
-    Friend WithEvents Button3 As Button
+    Friend WithEvents btnSort As Button
+    Friend WithEvents btnClear As Button
 End Class
--- Sorting Numbers with File Processing and LINQ/Form1.vb
+++ Sorting Numbers with File Processing and LINQ/Form1.vb
@@ -5,9 +5,8 @@ Public Class Form1
 
     Dim myFile As String = "numbers.txt"
 
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnWrite.Click
         Dim textInput As String = TextBox1.Text.Trim()
-        ' Simple check for input
         If String.IsNullOrEmpty(textInput) Then
             MessageBox.Show("Gotta put a number in first!", "Oops", MessageBoxButtons.OK, MessageBoxIcon.Information)
             TextBox1.Focus()
@@ -16,9 +15,7 @@ Public Class Form1
 
         Try
             Dim fileWriter As StreamWriter = New StreamWriter(myFile, True)
-
             fileWriter.WriteLine(textInput)
-
             fileWriter.Close()
 
             MessageBox.Show("Number saved!", "Done", MessageBoxButtons.OK, MessageBoxIcon.None)
@@ -29,7 +26,8 @@ Public Class Form1
         End Try
 
     End Sub
-    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles btnRead.Click
         ListBox1.Items.Clear()
 
         Try
@@ -51,27 +49,27 @@ Public Class Form1
         End Try
 
     End Sub
-    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles btnSort.Click
 
         If ListBox1.Items.Count = 0 Then
             MessageBox.Show("Read the numbers first by clicking READ!", "Wait", MessageBoxButtons.OK, MessageBoxIcon.Warning)
             Return
         End If
+
         Dim numbersList As New List(Of Integer)
 
         For Each item In ListBox1.Items
             Try
                 numbersList.Add(Convert.ToInt32(item.ToString()))
             Catch ex As FormatException
-                MessageBox.Show("Warning! Skipped bad data: " & item.ToString(), "Bad Data", MessageBoxButtons.OK, MessageBoxWarning)
+                MessageBox.Show("Warning! Skipped bad data: " & item.ToString(), "Bad Data", MessageBoxButtons.OK, MessageBoxIcon.Warning)
             End Try
         Next
 
         Console.WriteLine("--- Sorting numbers using LINQ OrderBy! ---")
         Dim sortedNumbers = numbersList.OrderBy(Function(n) n)
-
         Console.WriteLine(vbCrLf & "--- SORTED RESULTS in Console ---")
-
         For Each num In sortedNumbers
             Console.WriteLine(num)
         Next
@@ -79,8 +77,17 @@ Public Class Form1
         For Each num In sortedNumbers
             ListBox1.Items.Add(num.ToString())
         Next
-
         MessageBox.Show("Sorted results are now in the ListBox AND the Console!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+    End Sub
 
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        Try
+            Using writer As New StreamWriter(myFile, False)
+            End Using
+            ListBox1.Items.Clear()
+            MessageBox.Show($"Data in {myFile} has been cleared ", "File Cleared", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Catch ex As Exception
+            MessageBox.Show($"Error Clearing file: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
     End Sub
 End Class
